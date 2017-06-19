caec3a19c477cfeb72e578fb79c5b4e0151230b1
Naomi Navkal <navkal@hotmail.com>
2017-06-19 17:16:05 -0400

    Default users

--- CreateDB.py
+++ CreateDB.py
@@ -158,9 +158,9 @@ def make_database( bDestroy ):
 
     # Initialize default users
     cur.execute( '''INSERT OR IGNORE INTO User ( username, password, role_id, description ) VALUES (?,?,?,? )''', ('system', None, None, 'system') )
-    dbCommon.add_interactive_user( cur, conn, 'system', 'admin', 'admin', 'Administrator', 'Administrator', False )
-    dbCommon.add_interactive_user( cur, conn, 'system', 'tech', 'tech', 'Technician', 'Default Technician', False )
-    dbCommon.add_interactive_user( cur, conn, 'system', 'test', 'test', 'Visitor', 'Default Visitor', False )
+    dbCommon.add_interactive_user( cur, conn, 'system', 'admin', 'admin', 'Administrator', 'Administrator', False, True, 'Big', 'Bird', 'bb@sesame.com', 'Sesame Street' )
+    dbCommon.add_interactive_user( cur, conn, 'system', 'tech', 'tech', 'Technician', 'Default Technician', False, True, 'Cookie', 'Monster', 'chocochip@sesame.com', 'Sesame Street' )
+    dbCommon.add_interactive_user( cur, conn, 'system', 'test', 'test', 'Visitor', 'Default Visitor', False, True, 'Oscar', 'Grouch', 'trashcan@sesame.com', 'Sesame Street' )
 
     cur.execute('''INSERT INTO Activity ( timestamp, username, event_type, target_table, target_column, target_value, description )
         VALUES (?,?,?,?,?,?,? )''', ( time.time(), 'system', dbCommon.dcEventTypes['database'], '', '', '', 'Start generating database from CSV files' ) )
